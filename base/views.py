4d16204cb81226659afbfe9a5a1a1eb5ab6ffc85
dev <develorain@gmail.com>
2021-09-02 19:22:32 -0400

    Display city name for each city detail

--- base/views.py
+++ base/views.py
@@ -9,10 +9,11 @@ def city_list(request):
     return render(request, 'base/city_list.html', context)
 
 def city_detail(request, pk):
+    city = City.objects.all().only('name').get(id=pk).name
     restaurants = Restaurant.objects.all().filter(city_id=pk)
     restaurantfoods = RestaurantFood.objects.all().filter(restaurant_id__in=restaurants)
     #foods = Food.objects.all().filter(food_id__in=restaurantfoods) # not working
 
-    context = {'pk': pk, 'restaurants': restaurants, 'restaurantfoods': restaurantfoods}
+    context = {'pk': pk, 'city': city, 'restaurants': restaurants, 'restaurantfoods': restaurantfoods}
     
     return render(request, 'base/city_detail.html', context)
