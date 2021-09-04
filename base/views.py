35b8d7478099624df5ea13ea2f2974d216007745
dev <develorain@gmail.com>
2021-09-04 19:52:17 -0400

    Order query lists

--- base/views.py
+++ base/views.py
@@ -1,21 +1,15 @@
 from base.models import *
-
 from django.shortcuts import render
 from django.db.models.functions import Cast
 from django.db.models import IntegerField
 
 def city_list(request):
-    cities = City.objects.all()
+    cities = City.objects.raw('SELECT * FROM base_city ORDER BY name')
     context = {'cities': cities}
 
     return render(request, 'base/city_list.html', context)
 
 def city_detail(request, pk):
-    city = City.objects.filter(id=pk)
-    restaurants = Restaurant.objects.filter(city_id=pk)
-    restaurantfoods = RestaurantFood.objects.annotate(new_int_field=Cast('score', IntegerField())).order_by('-new_int_field', 'score')
-    foods = Food.objects.all()
-
     city = City.objects.raw('SELECT * FROM base_city WHERE id==' + str(pk))[0]
 
     restaurants_list = []
@@ -23,13 +17,13 @@ def city_detail(request, pk):
         restaurants_list.append(a)
     
     restaurantfoods_list = []
-    for a in RestaurantFood.objects.raw("SELECT * FROM base_restaurantfood WHERE restaurant_id IN (SELECT id FROM base_restaurant WHERE city_id==" + str(pk) + ")"):
+    for a in RestaurantFood.objects.raw("SELECT * FROM base_restaurantfood WHERE restaurant_id IN (SELECT id FROM base_restaurant WHERE city_id==" + str(pk) + ") ORDER BY score DESC"):
         restaurantfoods_list.append(a)
 
     foods_list = []
-    for a in Food.objects.raw("SELECT * FROM base_food WHERE id IN (SELECT food_id FROM base_restaurantfood WHERE restaurant_id IN (SELECT id FROM base_restaurant WHERE city_id==" + str(pk) + "))"):
+    for a in Food.objects.raw("SELECT * FROM base_food WHERE id IN (SELECT food_id FROM base_restaurantfood WHERE restaurant_id IN (SELECT id FROM base_restaurant WHERE city_id==" + str(pk) + ")) ORDER BY name"):
         foods_list.append(a)
 
-    context = {'pk': pk, 'city': city, 'restaurants': restaurants, 'restaurantfoods': restaurantfoods, 'foods': foods, 'city':city, 'restaurants_list':restaurants_list, 'restaurantfoods_list':restaurantfoods_list, 'foods_list':foods_list}
+    context = {'pk': pk, 'city':city, 'restaurants_list':restaurants_list, 'restaurantfoods_list':restaurantfoods_list, 'foods_list':foods_list}
     
     return render(request, 'base/city_detail.html', context)
