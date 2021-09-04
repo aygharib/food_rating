7a77681a6fdb72e6efd23234bb7819d7a8aa0518
dev <develorain@gmail.com>
2021-09-04 16:38:54 -0400

    Passing restaurantfoods in descending order in context

--- base/views.py
+++ base/views.py
@@ -1,6 +1,8 @@
 from base.models import *
 
 from django.shortcuts import render
+from django.db.models.functions import Cast
+from django.db.models import IntegerField
 
 def city_list(request):
     cities = City.objects.all()
@@ -9,20 +11,12 @@ def city_list(request):
     return render(request, 'base/city_list.html', context)
 
 def city_detail(request, pk):
-    # city = City.objects.get(id=pk).name #get city
-    # restaurants = Restaurant.objects.filter(city_id=pk) #restaurants in city
-    # restaurantfoods = RestaurantFood.objects.filter(restaurant_id__in=restaurants) #restaurantfoods in city
-
-    # restaurant_ids = Restaurant.objects.filter(city_id=pk).values('id') # city_id -> restaurant_id
-    # food_ids = RestaurantFood.objects.filter(restaurant_id__in=restaurant_ids).values('food_id') # restaurant_id -> food_id
-    # foods = Food.objects.filter(id__in=food_ids)
-
     # restaurants, restaurantfoods, foods
+    city = City.objects.filter(id=pk)
     restaurants = Restaurant.objects.filter(city_id=pk)
-    restaurantfoods = RestaurantFood.objects.all()
+    restaurantfoods = RestaurantFood.objects.annotate(new_int_field=Cast('score', IntegerField())).order_by('-new_int_field', 'score')
     foods = Food.objects.all()
-
-    # context = {'pk': pk, 'city': city, 'restaurants': restaurants, 'restaurantfoods': restaurantfoods, 'food_ids': food_ids}
-    context = {'pk': pk, 'restaurants': restaurants, 'restaurantfoods': restaurantfoods, 'foods': foods}
+    
+    context = {'pk': pk, 'city': city, 'restaurants': restaurants, 'restaurantfoods': restaurantfoods, 'foods': foods}
     
     return render(request, 'base/city_detail.html', context)
